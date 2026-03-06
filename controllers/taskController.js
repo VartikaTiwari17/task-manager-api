f5c718e092cafadcb8395c143ef58f21a9d102b0
Vartika Tiwari <vartikatiwari688@gmail.com>
2026-03-06 21:00:04 +0530

    Initial commit - Task Manager REST API

---
+++ controllers/taskController.js
@@ -0,0 +1,37 @@
+const Task = require("../models/Task");
+
+exports.createTask = async(req,res)=>{
+  const {title,description} = req.body;
+
+  if(!title){
+    return res.status(400).json({message:"Title required"});
+  }
+
+  const task = await Task.create({
+    title,
+    description,
+    user:req.user.id
+  });
+
+  res.json(task);
+};
+
+exports.getTasks = async(req,res)=>{
+  const tasks = await Task.find({user:req.user.id});
+  res.json(tasks);
+};
+
+exports.updateTask = async(req,res)=>{
+  const task = await Task.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new:true}
+  );
+
+  res.json(task);
+};
+
+exports.deleteTask = async(req,res)=>{
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({message:"Task deleted"});
+};
